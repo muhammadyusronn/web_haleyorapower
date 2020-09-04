2faf4202eee4c77bedad4224778caf39a20d717b
muhammadyusronn <muhammadyusron733@gmail.com>
2020-09-05 01:54:44 +0700

    Menambahkan database dan mengubah title page login

--- database/db_haleyorapower.sql
+++ database/db_haleyorapower.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Waktu pembuatan: 13 Agu 2020 pada 09.50
+-- Waktu pembuatan: 04 Sep 2020 pada 20.53
 -- Versi server: 10.4.13-MariaDB
 -- Versi PHP: 7.4.7
 
@@ -18,96 +18,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_persediaanbarang`
+-- Database: `db_haleyorapower`
 --
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `detailpembelian`
---
-
-CREATE TABLE `detailpembelian` (
-  `kodepembayaran` varchar(11) NOT NULL,
-  `kodebarang` varchar(20) NOT NULL,
-  `jumlahbarang` int(11) NOT NULL,
-  `hargabarang` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `detailpembelian`
---
-
-INSERT INTO `detailpembelian` (`kodepembayaran`, `kodebarang`, `jumlahbarang`, `hargabarang`) VALUES
-('PBL001', 'KBR001', 200, 5000),
-('PBL002', 'KBR001', 200, 5000);
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `detailpenjualan`
---
-
-CREATE TABLE `detailpenjualan` (
-  `kodepembayaran` varchar(11) NOT NULL,
-  `kodebarang` varchar(20) NOT NULL,
-  `jumlahbarang` int(11) NOT NULL,
-  `hargabarang` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `detailpenjualan`
---
-
-INSERT INTO `detailpenjualan` (`kodepembayaran`, `kodebarang`, `jumlahbarang`, `hargabarang`) VALUES
-('PNJ001', 'KBR001', 100, 9500);
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `pembelian`
---
-
-CREATE TABLE `pembelian` (
-  `kodepembayaran` varchar(20) NOT NULL,
-  `iddistributor` int(11) NOT NULL,
-  `tanggaltransaksi` date NOT NULL,
-  `pembayaran` int(11) NOT NULL,
-  `penanggungjawab` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `pembelian`
---
-
-INSERT INTO `pembelian` (`kodepembayaran`, `iddistributor`, `tanggaltransaksi`, `pembayaran`, `penanggungjawab`) VALUES
-('PBL001', 3, '2020-07-20', 1000000, 'admin'),
-('PBL002', 3, '2020-07-21', 1000000, 'admin');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `penjualan`
---
-
-CREATE TABLE `penjualan` (
-  `kodepembayaran` varchar(20) NOT NULL,
-  `nama` varchar(100) NOT NULL,
-  `alamat` text NOT NULL,
-  `nomorhp` varchar(13) NOT NULL,
-  `tanggaltransaksi` date NOT NULL,
-  `pembayaran` int(11) NOT NULL,
-  `penanggungjawab` varchar(50) NOT NULL,
-  `diskon` int(11) NOT NULL,
-  `totalbelanja` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `penjualan`
---
-
-INSERT INTO `penjualan` (`kodepembayaran`, `nama`, `alamat`, `nomorhp`, `tanggaltransaksi`, `pembayaran`, `penanggungjawab`, `diskon`, `totalbelanja`) VALUES
-('PNJ001', 'Fransiska', 'Jalan Angkatan 45 Lorong Harapan Palembang', '082186427595', '2020-07-21', 475000, 'admin', 50, 950000);
 
 -- --------------------------------------------------------
 
@@ -142,85 +54,65 @@ INSERT INTO `tbl_admin` (`username`, `password`, `nama`, `tanggallahir`, `alamat
 CREATE TABLE `tbl_barang` (
   `kodebarang` varchar(20) NOT NULL,
   `namabarang` varchar(100) NOT NULL,
-  `hargabarang` int(11) NOT NULL,
   `deskripsibarang` text NOT NULL,
   `jumlahbarang` int(11) NOT NULL,
-  `kodejenisbarang` varchar(20) NOT NULL
+  `satuan` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `tbl_barang`
 --
 
-INSERT INTO `tbl_barang` (`kodebarang`, `namabarang`, `hargabarang`, `deskripsibarang`, `jumlahbarang`, `kodejenisbarang`) VALUES
-('KBR001', 'Beras Mantap Nian', 9500, 'Ini adalah beras mantap nian', 200, 'KAT001');
+INSERT INTO `tbl_barang` (`kodebarang`, `namabarang`, `deskripsibarang`, `jumlahbarang`, `satuan`) VALUES
+('KBR001', 'Kabel TIC 35', 'Ini adalah kabel TIC 35', 80, 'Meter'),
+('KBR002', 'Sepatu Kabel AL-CU 70', 'Ini adalah sepatu kabel AL-CU 70', 82, 'Buah');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_distributor`
+-- Struktur dari tabel `tbl_detailtransaksi`
 --
 
-CREATE TABLE `tbl_distributor` (
-  `iddistributor` int(11) NOT NULL,
+CREATE TABLE `tbl_detailtransaksi` (
+  `kodetransaksi` varchar(20) NOT NULL,
+  `kodebarang` varchar(20) NOT NULL,
+  `jumlahbarang` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_detailtransaksi`
+--
+
+INSERT INTO `tbl_detailtransaksi` (`kodetransaksi`, `kodebarang`, `jumlahbarang`) VALUES
+('TRS001', 'KBR001', 6),
+('TRS001', 'KBR002', 6);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_transaksi`
+--
+
+CREATE TABLE `tbl_transaksi` (
+  `kodetransaksi` varchar(20) NOT NULL,
+  `tanggaltransaksi` date NOT NULL,
   `nama` varchar(100) NOT NULL,
-  `alamat` text NOT NULL,
-  `nomorhp` varchar(13) NOT NULL
+  `nomorhp` varchar(13) NOT NULL,
+  `pekerjaan` text NOT NULL,
+  `penanggungjawab` varchar(50) NOT NULL,
+  `penyulang` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `tbl_distributor`
+-- Dumping data untuk tabel `tbl_transaksi`
 --
 
-INSERT INTO `tbl_distributor` (`iddistributor`, `nama`, `alamat`, `nomorhp`) VALUES
-(3, 'Agen Sembako Jaya', 'Jalan Angkatan 45 Lorong Harapan Palembang', '081882823921');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `tbl_jenisbarang`
---
-
-CREATE TABLE `tbl_jenisbarang` (
-  `kodejenisbarang` varchar(20) NOT NULL,
-  `namajenisbarang` varchar(100) NOT NULL,
-  `deskripsijenisbarang` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `tbl_jenisbarang`
---
-
-INSERT INTO `tbl_jenisbarang` (`kodejenisbarang`, `namajenisbarang`, `deskripsijenisbarang`) VALUES
-('KAT001', 'Sembako', 'Ini adalah jenis barang sembako');
+INSERT INTO `tbl_transaksi` (`kodetransaksi`, `tanggaltransaksi`, `nama`, `nomorhp`, `pekerjaan`, `penanggungjawab`, `penyulang`) VALUES
+('TRS001', '2020-09-05', 'Henry Sujana', '082182823292', 'Pemasangan Pentahanan / Tombak Baru di Tiang CO Jaring KPW 1 Titik', 'admin', 'PIRAMID');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indeks untuk tabel `detailpembelian`
---
-ALTER TABLE `detailpembelian`
-  ADD PRIMARY KEY (`kodepembayaran`,`kodebarang`);
-
---
--- Indeks untuk tabel `detailpenjualan`
---
-ALTER TABLE `detailpenjualan`
-  ADD PRIMARY KEY (`kodepembayaran`,`kodebarang`);
-
---
--- Indeks untuk tabel `pembelian`
---
-ALTER TABLE `pembelian`
-  ADD PRIMARY KEY (`kodepembayaran`);
-
---
--- Indeks untuk tabel `penjualan`
---
-ALTER TABLE `penjualan`
-  ADD PRIMARY KEY (`kodepembayaran`);
 
 --
 -- Indeks untuk tabel `tbl_admin`
@@ -235,26 +127,16 @@ ALTER TABLE `tbl_barang`
   ADD PRIMARY KEY (`kodebarang`);
 
 --
--- Indeks untuk tabel `tbl_distributor`
+-- Indeks untuk tabel `tbl_detailtransaksi`
 --
-ALTER TABLE `tbl_distributor`
-  ADD PRIMARY KEY (`iddistributor`);
+ALTER TABLE `tbl_detailtransaksi`
+  ADD PRIMARY KEY (`kodetransaksi`,`kodebarang`);
 
 --
--- Indeks untuk tabel `tbl_jenisbarang`
+-- Indeks untuk tabel `tbl_transaksi`
 --
-ALTER TABLE `tbl_jenisbarang`
-  ADD PRIMARY KEY (`kodejenisbarang`);
-
---
--- AUTO_INCREMENT untuk tabel yang dibuang
---
-
---
--- AUTO_INCREMENT untuk tabel `tbl_distributor`
---
-ALTER TABLE `tbl_distributor`
-  MODIFY `iddistributor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `tbl_transaksi`
+  ADD PRIMARY KEY (`kodetransaksi`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
